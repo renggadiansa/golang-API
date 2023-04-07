0a9c2ce5b88d1b54d5c3c8239340445cb1aab163
apolzek <viniciusgomes955@gmail.com>
2023-04-07 09:46:23 -0300

    Add docker-compose file with mysql database

--- requests/user.request.go
+++ requests/user.request.go
@@ -3,8 +3,8 @@ package requests
 import "time"
 
 type UserRequest struct {
-	Name string `json:"name" form:"name" binding:"required"`
-	Email string `json:"email" binding:"required"`
-	Address string `json:"address" form:"address" binding:"required"`
+	Name     string    `json:"name" form:"name" binding:"required"`
+	Email    string    `json:"email" form:"email" binding:"required"`
+	Address  string    `json:"address" form:"address" binding:"required"`
 	BornDate time.Time `json:"born_date" form:"born_date" binding:"required"`
 }
