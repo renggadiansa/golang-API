2da29ea00ce1b6fbe16f7efa8161d0eaa1a9380c
renggadiansa <renggadiansa@gmail.com>
2023-04-08 09:26:31 +0700

    route v1

--- routes/index.route.go
+++ routes/index.route.go
@@ -3,31 +3,29 @@ package routes
 import (
 	"gin-goinc-api/configs/app_config"
 	"gin-goinc-api/controllers/book_controller"
-	filecontroller "gin-goinc-api/controllers/book_controller/file_controller"
-	"gin-goinc-api/controllers/user_controller"
-	"gin-goinc-api/middleware"
-
+	"gin-goinc-api/controllers/user_controller"	
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(app *gin.Engine) {
-	route := app
+	route := app.Group("")
 	route.Static(app_config.STATIC_ROUTE, app_config.STATIC_DIR)
 
 	//route user
-	route.GET("/user", user_controller.GetAllUser)
-	route.GET("/user/paginate", user_controller.GetUserPaginate)
-	route.POST("/user", user_controller.Store)
-	route.GET("/user/:id", user_controller.GetById)
-	route.PATCH("/user/:id", user_controller.UpdateById)
-	route.DELETE("/user/:id", user_controller.DeleteById)
+	userRoute := route.Group("user")
+	userRoute.GET("/", user_controller.GetAllUser)
+	userRoute.GET("/paginate", user_controller.GetUserPaginate)
+	userRoute.POST("/", user_controller.Store)
+	userRoute.GET("/:id", user_controller.GetById)
+	userRoute.PATCH("/:id", user_controller.UpdateById)
+	userRoute.DELETE("/:id", user_controller.DeleteById)
 
 
 	//route book
 	route.GET("/book", book_controller.GetAllBook)
 
-	//route controller file
-	route.POST("/file", filecontroller.HandleUploadFile)
-	route.DELETE("/file/:filename", middleware.AuthMiddlelware ,filecontroller.HandleRemoveFile)
+
+	v1Route(route)
+
 
 }
---
+++ routes/v1.route.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"gin-goinc-api/middleware"
+
+	"github.com/gin-gonic/gin"
+	
+	filecontroller "gin-goinc-api/controllers/book_controller/file_controller"
+)
+
+func v1Route(app *gin.RouterGroup) {
+	route := app
+
+	//route controller file
+	authRoute := route.Group("file", middleware.AuthMiddlelware)
+	authRoute.POST("/file",filecontroller.HandleUploadFile)
+	authRoute.DELETE("/:filename",filecontroller.HandleRemoveFile)
+}
